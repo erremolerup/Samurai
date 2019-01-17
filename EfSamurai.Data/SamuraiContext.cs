6833dc745170fa01d8f159819b7e7cedb2a58aa3
erremolerup <erika.molinder@gmail.com>
2019-01-17 13:45:42 +0100

    La till två klasser och la även till domain som referens till data

---
+++ EfSamurai.Data/SamuraiContext.cs
@@ -0,0 +1,19 @@
+﻿using EfSamurai.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfSamurai.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai;");
+        }
+    }
+}
